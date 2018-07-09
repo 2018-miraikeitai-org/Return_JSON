86b413060f6a2ff6e0f96e1f45f51637fd2e4d76
ryujiueno <ryuji.ueno.4e@stu.hosei.ac.jp>
2018-07-10 00:09:20 +0900

    net/httpでやりました！

--- Ryuji_Ueno/hello.go
+++ Ryuji_Ueno/hello.go
@@ -3,38 +3,29 @@ package main
 import(
 	"encoding/json"
 	"net/http"
-	"log"
-	"fmt"
 )
 
 type Message struct{
-	Result string
+	Result string `json:"Result"`
 }
 
 func main(){
 
-	message := Message{"Hello World"}
-	bdata := EncodingJSON(message)
-        http.ListenAndServe("5000",nil)
-	handleRequests()
+	http.HandleFunc("/",index)
+	http.ListenAndServe(":5000",nil)
 }
 
-func EncodingJSON(mes Message)[]byte{
-	bdata,err := json.Marshal(mes)
+func index(w http.ResponseWriter, r *http.Request){
+	result := Message{ Result:"Hello World"}
+	
+	res, err := json.Marshal(result)
+
 	if err != nil{
-		fmt.Println(err)
-		return nil
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
 	}
-	return bdata
-}
 
-func homePage(w http.ResponseWriter,r *http.Request){
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-   	fmt.Println("Endpoint Hit: homePage")
-}
-
-func handleRequests(){
-	http.HandleFunc("/",EncodingJSON(Message))
-	log.Fatal(http.ListenAndServe(":5000",nil))
+	w.Header().Set("Content-Type","application/json")
+	w.Write(res)
 }
 
